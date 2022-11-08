78264a6b9925570e6039469d7bed13d3c2d3ebb7
Robin-vdb <64130164+Robin-vdb@users.noreply.github.com>
2022-11-08 12:42:40 +0100

    Update EPP (1).py

--- pages/EPP (1).py
+++ pages/EPP (1).py
@@ -20,6 +20,27 @@ import streamlit
 
 dfMarker = pd.read_csv('Markermap.csv')
 
+st.code("m = folium.Map(location=[52.1009166, 5.6462914], tiles= None, zoom_start=7)
+basemap = folium.FeatureGroup(name = 'basemap', overlay = True, control = False)
+folium.TileLayer(tiles = 'OpenStreetMap').add_to(basemap)
+basemap.add_to(m)
+
+# Om het jaar te selecteren, hier alvast wat tips:
+# Maak per jaar folium featuregroups aan
+
+for i in range(1995,2022):
+    globals()['%s' %i] = folium.FeatureGroup(name=i, control = True, overlay = False)
+    m.add_child(globals()['%s' %i])        
+    for index, row in dfMarker.iterrows():
+        if str(row[str(i)]) != 'nan':
+            popup = row['Regio'] + '\n' +str(row[str(i)])
+            globals()['%s' %i].add_child(folium.Marker(location = [row['latitude'], row['longitude']], popup = popup)).add_to(m)
+
+folium.LayerControl(position = 'bottomleft', collapsed = False).add_to(m)   
+
+m")
+
+
 import folium 
 from folium import plugins
 
