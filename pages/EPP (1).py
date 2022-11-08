92ed068a8d52179528172397426b4ece772d9379
Robin-vdb <64130164+Robin-vdb@users.noreply.github.com>
2022-11-08 12:48:13 +0100

    Update EPP (1).py

--- pages/EPP (1).py
+++ pages/EPP (1).py
@@ -16,7 +16,7 @@ import numpy as np
 import streamlit_folium as st_folium
 from streamlit_folium import folium_static
 from folium import plugins
-import streamlit
+import streamlit as st
 
 dfMarker = pd.read_csv('Markermap.csv')
 
