b7af5de1e724dab5b0fd7c649c90b9d2cfc28e08
Robin-vdb <64130164+Robin-vdb@users.noreply.github.com>
2022-11-10 16:13:11 +0100

    Update EPP (1).py
    met clustermap

--- pages/EPP (1).py
+++ pages/EPP (1).py
@@ -26,27 +26,29 @@ from folium import plugins
 
 st.title('Gemiddelde verkoop map per jaar')
 
+
+#clustermap
 m = folium.Map(location=[52.1009166, 5.6462914], tiles= None, zoom_start=7)
 basemap = folium.FeatureGroup(name = 'basemap', overlay = True, control = False)
 folium.TileLayer(tiles = 'OpenStreetMap').add_to(basemap)
 basemap.add_to(m)
 
-# Om het jaar te selecteren, hier alvast wat tips:
-# Maak per jaar folium featuregroups aan
 
 for i in range(1995,2022):
-    globals()['%s' %i] = folium.FeatureGroup(name=i, control = True, overlay = False)
+    globals()['%s' %i] = folium.plugins.MarkerCluster(name=str(i), control = True, overlay = False)
     m.add_child(globals()['%s' %i])        
     for index, row in dfMarker.iterrows():
         if str(row[str(i)]) != 'nan':
             popup = row['Regio'] + '\n' +str(row[str(i)])
             globals()['%s' %i].add_child(folium.Marker(location = [row['latitude'], row['longitude']], popup = popup)).add_to(m)
 
+
 folium.LayerControl(position = 'bottomleft', collapsed = False).add_to(m)   
+
 
 m
 
-a = folium_static(m)
+
 
 
 st.code('''m = folium.Map(location=[52.1009166, 5.6462914], tiles= None, zoom_start=7)
@@ -54,11 +56,8 @@ basemap = folium.FeatureGroup(name = 'basemap', overlay = True, control = False)
 folium.TileLayer(tiles = 'OpenStreetMap').add_to(basemap)
 basemap.add_to(m)
 
-# Om het jaar te selecteren, hier alvast wat tips:
-# Maak per jaar folium featuregroups aan
-
 for i in range(1995,2022):
-    globals()['%s' %i] = folium.FeatureGroup(name=i, control = True, overlay = False)
+    globals()['%s' %i] = folium.plugins.MarkerCluster(name=str(i), control = True, overlay = False)
     m.add_child(globals()['%s' %i])        
     for index, row in dfMarker.iterrows():
         if str(row[str(i)]) != 'nan':
