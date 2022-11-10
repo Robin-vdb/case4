563d552aa6834e92401a96b477e3d38dfe3c0e88
WesselMooijHVA <114337239+WesselMooijHVA@users.noreply.github.com>
2022-11-10 16:13:48 +0100

    Update and rename page2.py to conclusie.py

---
+++ pages/conclusie.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.title('kleine fluctuaties huizenmarkt')
+st.write('na de crisis 2008 (Lehman Brothers).')
+st.write('veel huizen bijgebouwd tussen 2009 en 2015')
+st.write('de jubelton is in 2013 in het leven geroepen (wordt in 2023 afgeschaft ook weer voor de huizenmarkt)')
+st.write('inflatie')
+
+st.title('huurprijs')
+st.write('de huurprijs stijgt met de huizenprijs mee. Maar huurharmonisatie werkt ook niet mee')
+st.write('juli 2021 tm juli 2021 geen stijging')
+
+st.title('Woontevredenheid')
+st.write('Mensen in Noor- en Zuid-holland die veel meer voor een huis betalen zijn er minder blij met hun huis.')
+st.write('wel iets gestegen ten opzichte van 2018 (landelijk)')
+
+st.title('bronnenlijst')
+st.write('Ministerie van Algemene Zaken. (2022, November 9). Wat is de maximale huurverhoging in 2021? Rijksoverheid.nl. 
+         'https://www.rijksoverheid.nl/onderwerpen/woning-huren/vraag-en-antwoord/wat-is-de-maximale-huurverhoging-in-2021')
+st.write('Centraal Bureau voor de Statistiek. (2022, June 9). Tevredenheid met woning licht opgelopen. 
+         'Centraal Bureau Voor De Statistiek. https://www.cbs.nl/nl-nl/nieuws/2022/23/tevredenheid-met-woning-licht-opgelopen')
